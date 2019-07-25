c63b0842a4883bc3faeec9a552e32097affdc8c1
wb <wb2276wzy@163.com>
2019-07-25 22:09:48 +0800

    update test.java

--- test-git/src/main/java/com/baizhi/Test.java
+++ test-git/src/main/java/com/baizhi/Test.java
@@ -5,5 +5,6 @@ package com.baizhi;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("好吃不过饺子，好玩不过嫂子！");
     }
 }
